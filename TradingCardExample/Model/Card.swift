dc15376a78889bad385b621dd91bc10b8065734e
Isaad Khan <IKhan@lcs.on.ca>
2023-01-17 10:11:30 -0500

    Made several instances of the structure

--- TradingCardExample/Model/Card.swift
+++ TradingCardExample/Model/Card.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 struct Card {
+    let emoji: String
     let description: String
     let age: String
     let height: String
@@ -16,3 +17,9 @@ struct Card {
     let color: Color
     let name: String
 }
+
+let mrSunglasses = Card(emoji: "😎", description: "Having a sunny disposition, Mr. Sunglasses likes to cruise the highways of California in his vintage convertible.", age: "27", height: "180 cm", sign: "Pisces", color: Color("Peach"), name: "Mr. Sunglasses")
+
+let laughingPerson = Card(emoji: "😂", description: "A person who loves to laugh at anything.", age: "16", height: "177 cm", sign: "Aquarias", color: Color("lime"), name: "Mr. Joyous")
+
+let angryPerson = Card(emoji: "🤬", description: "Gets angry at the slightest disturbance, has a potty mouth.", age: "34", height: "156 cm", sign: "Aries", color: Color("Sky"), name: "Mr. Angry")
